de15ec7535aaefd84f13dbb0006d0645ad8ac0e7
Anand-Chavan <anand2431997@gmail.com>
2022-10-14 14:29:35 +0530

    Add swagger

--- app.js
+++ app.js
@@ -6,6 +6,13 @@ const port = args[1].split('=')[1];
 const env = args[0]
 express.use(cors())
 express.use(app.json())
+fs = require('fs')
+
+const swaggerUi = require('swagger-ui-express')
+const swaggerFile = require('./swagger/swagger-output.json')
+
+express.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile))
+
 express.get('/',(req,res)=>{
     res.send('Welcome to movie db app '+ req.user);
 })
